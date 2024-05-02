3086a9802ec5938e13a105a45f28556e4f1810a6
Batu YURT <asdasd018@hotmail.com>
2024-05-03 00:01:55 +0300

    SaveChangeAsync Overrided

--- Persistence/Contexts/BenYaparimDbContext.cs
+++ Persistence/Contexts/BenYaparimDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Entities;
+﻿using Core.DataAccess;
+using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -20,7 +21,7 @@ namespace Persistence.Contexts
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            
+
             base.OnConfiguring(optionsBuilder);
         }
 
@@ -38,6 +39,23 @@ namespace Persistence.Contexts
             modelBuilder.Entity<OperationClaim>().HasData(operationClaims);
 
             base.OnModelCreating(modelBuilder);
+        }
+
+        public async override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var datas = ChangeTracker.Entries<Entity>();
+
+            foreach (var item in datas)
+            {
+                var result = item.State switch
+                {
+                    EntityState.Added => item.Entity.CreatedDate = DateTime.UtcNow,
+                    EntityState.Modified => item.Entity.UpdatedDate = DateTime.UtcNow,
+                    _ => DateTime.UtcNow,
+                };
+            }
+
+            return await base.SaveChangesAsync(cancellationToken);
         }
 
     }
